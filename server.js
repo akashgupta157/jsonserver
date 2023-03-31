a0c7c456080c493bd8bde08376d8ee5500036bc3
Your Name <you@example.com>
2023-03-31 12:26:11 +0530

    a

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-// server.js
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const PORT = process.env.PORT || 8000
 
-server.use(middlewares);
-server.use("", router);
-server.listen(process.env.PORT || 5000, () => {
-  console.log("JSON Server is running");
-});
-
+server.use(middlewares)
+server.use(router)
+server.listen(PORT, () => {
+  console.log('JSON Server is running')
+})
